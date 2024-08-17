b2c8338ab2d64cdc2cc9268d8129c8b3b98e3fbd
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-08-17 12:24:26 +0200

    task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+export default function handleResponseFromAPI(promise) {
+promise
+  .then(() => {
+    const attr = {
+      status: 200,
+      body: 'success',
+    };
+    return attr;
+  })
+  .catch(() => {
+    const error = new Error('');
+    return error;
+  })
+  .finally(() => {
+    console.log('Got a response from the API')
+  })
+}
