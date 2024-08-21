8aca6198736d20764db0d032601f485b57e5ccb2
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-08-22 00:21:03 +0200

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const hp = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return hp;
+}
+export default handleProfileSignup;
