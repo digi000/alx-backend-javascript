a11ec1ea5a70b35a4f31c054d21b39fb0d8b363f
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-08-21 22:16:28 +0200

    task 3 error review

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResult, userResult]) => {
-      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
-    })
-    .catch(() => {
+async function handleProfileSignup() {
+  try {
+    const [photoResult, userResult] = await Promise.all([uploadPhoto(), createUser()])
+    console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+  }catch(error) {
       console.error('Signup system offline');
-    });
+  }
 }
+export default handleProfileSignup;
