3810f6c648fe0975aa1adeed7eec1c6670176918
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-08-21 22:21:03 +0200

    task 3 error review lint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,10 @@ import { uploadPhoto, createUser } from './utils';
 
 async function handleProfileSignup() {
   try {
-    const [photoResult, userResult] = await Promise.all([uploadPhoto(), createUser()])
+    const [photoResult, userResult] = await Promise.all([uploadPhoto(), createUser()]);
     console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
-  }catch(error) {
-      console.error('Signup system offline');
+  } catch (error) {
+    console.error('Signup system offline');
   }
 }
 export default handleProfileSignup;
