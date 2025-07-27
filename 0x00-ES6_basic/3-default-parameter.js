a0aa8c7a22cb301a5538998e359c89b5ce4ed403
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-07-27 22:58:27 +0200

    task 3

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,7 @@
+/*
+ * Condensed the internals of a standard function to 1 line
+ */
+
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+	return initialNumber + expansion1989 + expansion2019
+}
